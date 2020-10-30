b9608948be176ae557f07afc85bb5764d5be0e16
Epiphany-D <857792565@qq.com>
2020-10-30 17:28:48 +0800

    2020年10月30日17:21:58

---
+++ DataStruct/DataStruct/Tree.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+using namespace std;
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -1
+#define MAX(a,b) (a)>(b)?(a):(b)
+#define MIN(a,b) (a)>(b)?(b):(a)
+typedef int  Status;
+
+
+
+    
+int main() {
+    system("pause");
+    return 0;
+}
