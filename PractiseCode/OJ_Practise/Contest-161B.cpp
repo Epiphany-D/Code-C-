152a8b3f4a2e3bb3ee0d27e4b8bed26cdf95563b
Epiphany-D <857792565@qq.com>
2020-12-30 11:42:51 +0800

    e

--- PractiseCode/OJ_Practise/Contest-161B.cpp
+++ PractiseCode/OJ_Practise/Contest-161B.cpp
@@ -18,56 +18,83 @@
 // 1
 
 //装箱问题，因为高度统一不在考虑范围内，该问题等同于在6*6平面内画正方形
-#include <stdio.h>
-#include <stdlib.h>
+
 #include <iostream>
+
 using namespace std;
-const int NUM = 6;
-int res[NUM];
+
 int main() {
-    int x6, x5, x4, x3, x2, x1;
-    cin >> x1 >> x2 >> x3 >> x4 >> x5 >> x6;
-    while (x1 + x2 + x3 + x4 + x5 + x6 > 0) {
-        for (int i = 0; i < NUM; i++) res[i] = 0;
-        int sum = 0;
+    int a, b, c, d, e, f;
+    int x, y;  //x为可以放1*1箱子的空余位置，y为可以放2*2箱子的空余位置
+    int cl[4] = {0, 5, 3, 1};
+    int sum = 0;
 
-        sum += x6;
-
-        sum += x5;
-        res[0] -= 11 * x5;
-
-        sum += x4;
-        res[1] -= 5 * x4;
-
-        sum += x3 / 4;
-        int tmp = x3 % 4;
-        if (tmp == 1) {
-            sum += 1;
-            res[1] -= 5;
-            res[0] -= 7;
-        } else if (tmp == 2) {
-            sum += 1;
-            res[1] -= 3;
-            res[0] -= 6;
-        } else if (tmp == 3) {
-            sum += 1;
-            res[1] -= 1;
-            res[0] -= 5;
-        }
-
-        if (x2 + res[1] <= 0)
-            res[0] += 4 * (res[1] + x2);
-        else {
-            sum += (x2 + res[1] + 8) / 9;
-            res[0] -= (9 - (x2 + res[1]) % 9) * 4;
-        }
-
-        if (x1 + res[0] > 0)
-            sum += (x1 + res[0] + 35) / 36;
-
+    while (1) {
+        cin >> a >> b >> c >> d >> e >> f;
+        if (a == 0 && b == 0 && c == 0 && d == 0 && e == 0 && f == 0)
+            break;
+        sum = d + e + f + (c + 3) / 4;  // c+3为向上取整
+        y = 5 * d + cl[c % 4];
+        if (b > y)
+            sum += (b - y + 8) / 9;  //+8向上取整
+        x = 36 * sum - 36 * f - 25 * e - 16 * d - 9 * c - 4 * b;
+        if (a > x)
+            sum += (a - x + 35) / 36;  //+35向上取整
         cout << sum << endl;
-
-        cin >> x1 >> x2 >> x3 >> x4 >> x5 >> x6;
     }
     return 0;
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <iostream>
+// using namespace std;
+// const int NUM = 6;
+// int res[NUM];
+// int main() {
+//     int x6, x5, x4, x3, x2, x1;
+//     cin >> x1 >> x2 >> x3 >> x4 >> x5 >> x6;
+//     while (x1 + x2 + x3 + x4 + x5 + x6 > 0) {
+//         for (int i = 0; i < NUM; i++) res[i] = 0;
+//         int sum = 0;
+
+//         sum += x6;
+
+//         sum += x5;
+//         res[0] -= 11 * x5;
+
+//         sum += x4;
+//         res[1] -= 5 * x4;
+
+//         sum += x3 / 4;
+//         int tmp = x3 % 4;
+//         if (tmp == 1) {
+//             sum += 1;
+//             res[1] -= 5;
+//             res[0] -= 7;
+//         } else if (tmp == 2) {
+//             sum += 1;
+//             res[1] -= 3;
+//             res[0] -= 6;
+//         } else if (tmp == 3) {
+//             sum += 1;
+//             res[1] -= 1;
+//             res[0] -= 5;
+//         }
+
+//         if (x2 + res[1] <= 0)
+//             res[0] += 4 * (res[1] + x2);
+//         else {
+//             sum += (x2 + res[1] + 8) / 9;
+//             res[0] -= (9 - (x2 + res[1]) % 9) * 4;
+//         }
+
+//         if (x1 + res[0] > 0)
+//             sum += (x1 + res[0] + 35) / 36;
+
+//         cout << sum << endl;
+
+//         cin >> x1 >> x2 >> x3 >> x4 >> x5 >> x6;
+//     }
+//     return 0;
+// }
